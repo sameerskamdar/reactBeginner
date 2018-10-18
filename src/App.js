ab6499fd1516de384f072c214fd34e57ac71c3de
Sameer Kamdar <Sameer.Kamdar@bentley.com>
2018-10-18 17:27:53 +0530

    more concepts added

--- src/App.js
+++ src/App.js
@@ -2,16 +2,18 @@ import React, { Component } from 'react';
 import './App.css';
 import Person from './Person/Person'
 class App extends Component {
+  
   state = {
     persons: [
       { club:'barcelona', position:'1'},
       { club:'real madrid', position:'2'},
       { club:'atletico madrid', position:'3'},
       { club:'sevilla', position:'4'}
-    ]
+    ],
+    showClubs: false
   }
 
-  switchClubName = () =>{
+  switchClubName = (newClub) =>{
     //console.log('clicked');
     //this.state.persons[1].club = 'RM';
     this.setState({
@@ -19,20 +21,60 @@ class App extends Component {
         { club:'barcelona', position:'1'},
         { club:'RM', position:'2'},
         { club:'atletico madrid', position:'3'},
-        { club:'sevilla', position:'5'}
+        { club: newClub, position:'5'}
       ]
     })
   }
 
+  clubChanged = (event) => {
+    this.setState({
+      persons: [
+        { club:'barcelona', position:'1'},
+        { club:'RM', position:'2'},
+        { club:'atletico madrid', position:'3'},
+        { club: event.target.value, position:'5'}
+      ]
+    })
+  }
+
+  toggleClubs = () =>
+  {
+    const doesShow = this.state.showClubs;
+    this.setState({
+      showClubs: !doesShow
+    });
+  }
+
   render() {
+    const style = {
+      backgroundColor: 'white',
+      font: 'inherit',
+      border: '1px solid blue',
+      padding: '8px'
+    }
+    let persons = null;
+
+        if(this.state.showClubs){
+          persons = (
+            <div>
+
+            {
+              this.state.persons.map( person => {
+                return <Person club={person.club} position={person.position}/>
+              })
+              /* <Person club={this.state.persons[0].club} position={this.state.persons[0].position}/> 
+            <Person club={this.state.persons[1].club} position={this.state.persons[1].position} click={this.switchClubName.bind(this, 'girona')}/> 
+            <Person club={this.state.persons[2].club} position={this.state.persons[2].position}> 2nd fav </Person> 
+            <Person club={this.state.persons[3].club} position={this.state.persons[3].position} changed={this.clubChanged}/> */
+            }
+          </div>
+          );
+        }
     return (
       <div className="App">
         <h1>Hi, React</h1>
-        <button onClick={this.switchClubName}>switch name</button>
-        <Person club={this.state.persons[0].club} position={this.state.persons[0].position}/> 
-        <Person club={this.state.persons[1].club} position={this.state.persons[1].position} click={this.switchClubName}/> 
-        <Person club={this.state.persons[2].club} position={this.state.persons[2].position}> 2nd fav </Person> 
-        <Person club={this.state.persons[3].club} position={this.state.persons[3].position}/>
+        <button onClick={this.toggleClubs} style={style}>toggle</button>
+        {persons}
       </div>
     );
   }
--- src/Person/Person.js
+++ src/Person/Person.js
@@ -1,11 +1,13 @@
 import React from 'react';
+import './Person.css';
 const person = (props) => {
     return (
-        <div>
+        <div className='Person'>
             <p onClick={props.click}> club: {props.club} and 
             rank: {props.position}
             </p>
             <p>{props.children}</p>
+            <input type='text' onChange={props.changed} value={props.club}/>
         </div>
     )
 };
