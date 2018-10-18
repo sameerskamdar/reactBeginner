2cb7ec0068999ddb7c2003b1dc5679e2e9aca527
Sameer Kamdar <Sameer.Kamdar@bentley.com>
2018-10-18 17:45:43 +0530

    Updates

---
+++ precache-manifest.17b1f54b60eacdccb72d662f0dde012b.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "938984662d1ace2e4d2a",
+    "url": "/reactBeginner/static/js/runtime~main.93898466.js"
+  },
+  {
+    "revision": "ac95020448e55a5f479a",
+    "url": "/reactBeginner/static/js/main.ac950204.chunk.js"
+  },
+  {
+    "revision": "7eae6140ad0adf38fbdc",
+    "url": "/reactBeginner/static/js/1.7eae6140.chunk.js"
+  },
+  {
+    "revision": "ac95020448e55a5f479a",
+    "url": "/reactBeginner/static/css/main.b9eaae26.chunk.css"
+  },
+  {
+    "revision": "9f81d0804708ff3f4e4eee046b116e6b",
+    "url": "/reactBeginner/index.html"
+  }
+];
